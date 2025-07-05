e82b4818dc0b527ca91e848cedf0d73ad6535c33
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-07-05 15:25:45 -0300

    feat: ensure password and password confirmation are the same on sign up form

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -28,32 +28,42 @@ import { Input } from '@/components/ui/input'
 import PasswordInput from '@/components/ui/password-input'
 import { api } from '@/lib/axios'
 
-const signupSchema = z.object({
-  firstName: z.string().trim().min(1, {
-    message: 'O nome é obrigatório',
-  }),
-  lastName: z.string().trim().min(1, {
-    message: 'O sobrenome é obrigatório',
-  }),
-  email: z
-    .string()
-    .trim()
-    .min(1, {
-      message: 'O e-mail é obrigatório',
-    })
-    .email({
-      message: 'O email é inválido',
+const signupSchema = z
+  .object({
+    firstName: z.string().trim().min(1, {
+      message: 'O nome é obrigatório',
     }),
-  password: z.string().trim().min(6, {
-    message: 'A senha precisa ter pelo menos 6 caracteres',
-  }),
-  passwordConfirmation: z.string().trim().min(6, {
-    message: 'A confirmação de senha é obrigatória',
-  }),
-  terms: z.boolean().refine((value) => value === true, {
-    message: 'Voce precisa aceitar os termos de uso',
-  }),
-})
+    lastName: z.string().trim().min(1, {
+      message: 'O sobrenome é obrigatório',
+    }),
+    email: z
+      .string()
+      .trim()
+      .min(1, {
+        message: 'O e-mail é obrigatório',
+      })
+      .email({
+        message: 'O email é inválido',
+      }),
+    password: z.string().trim().min(6, {
+      message: 'A senha precisa ter pelo menos 6 caracteres',
+    }),
+    passwordConfirmation: z.string().trim().min(6, {
+      message: 'A confirmação de senha é obrigatória',
+    }),
+    terms: z.boolean().refine((value) => value === true, {
+      message: 'Voce precisa aceitar os termos de uso',
+    }),
+  })
+  .refine(
+    (data) => {
+      return data.password === data.passwordConfirmation
+    },
+    {
+      message: 'As senhas precisam ser iguais',
+      path: ['passwordConfirmation'],
+    }
+  )
 
 const SignupPage = () => {
   const [user, setUser] = useState(null)
