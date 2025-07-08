c54ab53272e7813e824d733e956ebd53d063ca74
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-07-08 17:06:52 -0300

    refactor: add user service with login method

--- src/services/user.jsx
+++ src/services/user.jsx
@@ -10,4 +10,11 @@ export const UserService = {
     })
     return response.data
   },
+  login: async (input) => {
+    const response = await publicApi.post('/users/auth/login', {
+      email: input.email,
+      password: input.password,
+    })
+    return response.data
+  },
 }
