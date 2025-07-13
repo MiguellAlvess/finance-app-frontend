b279790d17d0290be39da6bdd2d44530d3c96732
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-07-13 15:21:27 -0300

    fix: ensure from and to params are only consired if they are valid

--- src/components/date-selection.jsx
+++ src/components/date-selection.jsx
@@ -1,4 +1,5 @@
 import { useQueryClient } from '@tanstack/react-query'
+import { addMonths, isValid } from 'date-fns'
 import { format } from 'date-fns'
 import { useEffect, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router'
@@ -7,25 +8,37 @@ import { useAuthContext } from '@/context/auth'
 
 import { DatePickerWithRange } from './ui/date-picker-with-range'
 
-const formatDateToQueryParam = (date) => {
-  return format(date, 'yyyy-MM-dd')
+const formatDateToQueryParam = (date) => format(date, 'yyyy-MM-dd')
+
+const getInitialDateState = (searchParams) => {
+  const defaultDate = {
+    from: new Date(),
+    to: addMonths(new Date(), 1),
+  }
+  const from = searchParams.get('from')
+  const to = searchParams.get('to')
+  if (!from || !to) {
+    return defaultDate
+  }
+  const datesAreInvalid = !isValid(new Date(from)) || !isValid(new Date(to))
+
+  if (datesAreInvalid) {
+    return defaultDate
+  }
+  return {
+    from: new Date(from + 'T00:00:00'),
+    to: new Date(to + 'T00:00:00'),
+  }
 }
 
 const DateSelection = () => {
-  const [searchParams] = useSearchParams()
-  const { user } = useAuthContext()
   const queryClient = useQueryClient()
+  const [searchParams] = useSearchParams()
   const navigate = useNavigate()
-  const [date, setDate] = useState({
-    from: searchParams.get('from')
-      ? new Date(searchParams.get('from') + 'T00:00:00')
-      : new Date(),
-    to: searchParams.get('to')
-      ? new Date(searchParams.get('to') + 'T00:00:00')
-      : new Date(),
-  })
+  const { user } = useAuthContext()
+  const [date, setDate] = useState(getInitialDateState(searchParams))
   useEffect(() => {
-    if (!date?.from && !date?.to) return
+    if (!date?.from || !date?.to) return
     const queryParams = new URLSearchParams()
     queryParams.set('from', formatDateToQueryParam(date.from))
     queryParams.set('to', formatDateToQueryParam(date.to))
@@ -39,6 +52,7 @@ const DateSelection = () => {
       ],
     })
   }, [navigate, date, queryClient, user.id])
+
   return <DatePickerWithRange value={date} onChange={setDate} />
 }
 
