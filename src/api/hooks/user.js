b8b048d797ca5bc93badbd611b0cbdf615d78ff1
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-07-15 17:24:56 -0300

    refactor: add hook  thtat gets user balance

---
+++ src/api/hooks/user.js
@@ -0,0 +1,20 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { UserService } from '@/api/services/user'
+import { useAuthContext } from '@/context/auth'
+
+export const getUserBalanceQueryKey = ({ from, to, userId }) => {
+  return ['balance', userId, from, to]
+}
+
+export const useGetUserBalance = ({ from, to }) => {
+  const { user } = useAuthContext()
+  return useQuery({
+    queryKey: getUserBalanceQueryKey({ from, to, userId: user.id }),
+    queryFn: () => {
+      return UserService.getBalance({ from, to })
+    },
+    staleTime: 1000 * 60 * 5,
+    enabled: Boolean(from) && Boolean(to) && Boolean(user.id),
+  })
+}
