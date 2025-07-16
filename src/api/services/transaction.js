8fb24cd6e4a25e346972fb9626049901bbb27861
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-07-16 17:17:30 -0300

    feat: add update transaction service and hooks functions

--- src/api/services/transaction.js
+++ src/api/services/transaction.js
@@ -4,12 +4,26 @@ import { protectedApi } from '@/lib/axios'
 
 export const TransactionService = {
   create: async (input) => {
-    const response = await protectedApi.post('/transactions/me', input)
+    const response = await protectedApi.post('/transactions/me', {
+      amount: input.amount,
+      name: input.name,
+      date: input.date,
+      type: input.type,
+    })
     return response.data
   },
   getAll: async (input) => {
     const query = queryString.stringify({ from: input.from, to: input.to })
     const response = await protectedApi.get(`/transactions/me?${query}`)
+    return response.data
+  },
+  update: async (input) => {
+    const response = await protectedApi.patch(`/transactions/me${input.id}`, {
+      amount: input.amount,
+      name: input.name,
+      date: input.date,
+      type: input.type,
+    })
     return response.data
   },
 }
